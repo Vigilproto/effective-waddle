1c3598b28488f372de0c0c5fe171ddad45273064
Vigilproto <100791896+Vigilproto@users.noreply.github.com>
2022-07-19 12:30:47 -0500

    Add files via upload

---
+++ First program.py
@@ -0,0 +1,41 @@
+#This is my first program
+
+
+
+from cgitb import text
+from time import sleep
+from tkinter.messagebox import QUESTION
+
+
+print("Hello how are you I am a program and my creator is Jonathan")
+sleep(2)
+
+print("This program is the first program ever created by my creator")
+
+sleep(2)
+
+print("I dont have a name but I would like to know yours.")
+
+sleep(2)
+
+input("Whats your name?")
+
+print("Thats a cool name")
+
+sleep(2)
+
+print("I wished I had a name")
+
+sleep(2)
+
+print ("Wait I have an idea why dont you give me one ")
+
+sleep(2)
+
+input("What would you like to name me?")
+
+print("Thats a cool name. Thanks!")
+
+sleep(2)
+
+print("Have a good day!")
